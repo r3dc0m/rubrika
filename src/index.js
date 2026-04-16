15b43635f6b9c87d6b23d34624297c05d4184af4
saray <saragaby314@gmail.com>
2026-04-16 19:11:27 +0200

    feat: actualización de archivos

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes/router.js";
+import { checkDB,syncDB } from "./config/db.js";
+
+dotenv.config();
+const PORT = process.env.APP_PORT;
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+
+app.use("/",router);
+app.get("/",(req,res)=>{
+    res.send("hello world");
+})
+
+checkDB();
+syncDB();
+app.listen(PORT,()=>{
+    console.log(`Servidor en marcha en puerto ${PORT}`);
+})
