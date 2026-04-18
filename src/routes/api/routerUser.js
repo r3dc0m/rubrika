e0ae9927a622e91bec5238799af46b46dfa0d5f9
r3dc0m <jonathanvelez85@gmail.com>
2026-04-18 10:26:20 +0200

    0418.1025 base rutas (faltan correlaciones de claves foráneas). Vistas pendientes para futuro

--- src/routes/api/routerUser.js
+++ src/routes/api/routerUser.js
@@ -2,7 +2,15 @@ import { Router } from "express";
 import userController from "../../controllers/api/userController.js"
 const routerUser = Router();
 
+<<<<<<< Updated upstream
 routerUser.get("/",userController.getAllUsers);
+=======
+routerUser.get("/users",userController.getAllUsers);
+
+export default routerUser;
+
+
+>>>>>>> Stashed changes
 /*
 routerUser.get("/:id",userController.getRideById)
 
@@ -13,5 +21,9 @@ routerUser.put("/:id",userController.updateRide);
 routerUser.patch("/:id/status",userController.setStatus);
 
 routerUser.delete("/:id",userController.deleteRide);
+<<<<<<< Updated upstream
 */
 export default routerUser;
+=======
+*/
+>>>>>>> Stashed changes
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,17 @@
 import {Router} from "express";
-import apiRouter from "./api/apiRouter.js";
-import viewRouter from "./view/viewRouter.js";
+import appRouter from "./api/appRouter.js";
+import routerUser from "./api/routerUser.js";
 
 
 const router = Router();
 
+<<<<<<< Updated upstream
 
 //router.use("/",viewRouter);
 router.use("/api",apiRouter);
+=======
+router.use("/api",appRouter); //para modelo de claves foraneas. no usar de momento!
+router.use("/api",routerUser);
+>>>>>>> Stashed changes
 
 export default router;
