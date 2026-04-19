e55caeab8d7e96e6542dc39e99f183339afcff6b
saray <saragaby314@gmail.com>
2026-04-19 20:44:46 +0200

    feat: añadiendo criteria y evaluations con model, controller y routes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/router.js";
-import { checkDB,syncDB } from "./config/db.js";
+import { checkDB, syncDB } from "./config/db.js";
 
 dotenv.config();
-const PORT = process.env.APP_PORT;
+
+const PORT = process.env.PORT;
 const app = express();
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/", router);
 
-app.use("/",router);
-app.get("/",(req,res)=>{
-    res.send("hello world");
-})
+app.get("/", (req, res) => {
+    res.send("Rubrika API funcionando");
+});
 
-checkDB();
-//syncDB();
-app.listen(PORT,()=>{
-    console.log(`Servidor en marcha en puerto ${PORT}`);
-})
+// Primero conectamos la BD, luego arrancamos el servidor
+async function startServer() {
+    await checkDB();
+    await syncDB();
+    app.listen(PORT, () => {
+        console.log(`Servidor en marcha en puerto ${PORT}`);
+    });
+}
+
+startServer();
