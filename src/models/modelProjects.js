7d56ebfdab67f3a685781ea5943c7f002bed5065
Olatz <olatzglez@gmail.com>
2026-04-22 09:31:51 +0200

    Restaurar modelProjects.js desde origin/views

--- src/models/modelProjects.js
+++ src/models/modelProjects.js
@@ -12,27 +12,11 @@ const projectModel = sequelize.define("projects", {
         allowNull: false
     },
     project_name: {
-<<<<<<< HEAD
-<<<<<<< HEAD:src/models/projectModel.js
-        type: DataTypes.CHAR(34),
-        allowNull: false
-    },
-    repo_link: {
-        type: DataTypes.CHAR(55),
-=======
         type: DataTypes.STRING(34),
         allowNull: false
     },
     repo_link: {
         type: DataTypes.STRING(55),
->>>>>>> 1627ab8 (0420.1646):src/models/modelProjects.js
-=======
-        type: DataTypes.VARCHAR(34),
-        allowNull: false
-    },
-    repo_link: {
-        type: DataTypes.VARCHAR(55),
->>>>>>> 19bb8be (Cambios en los modelos: projects y evaluations_criteria)
         allowNull: true
     }
 }, {
