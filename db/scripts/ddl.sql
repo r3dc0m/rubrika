846c8d6a464907de1f5cfe943bc759419f2efd3c
Olatz <olatzglez@gmail.com>
2026-04-22 16:28:57 +0200

    Cambios en db/scripts/dd.sql y src/config.db.js

--- db/scripts/ddl.sql
+++ db/scripts/ddl.sql
@@ -98,8 +98,8 @@ CREATE TABLE IF NOT EXISTS public.users
 (
     user_id integer NOT NULL,
     name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(255) COLLATE pg_catalog."default" NOT NULL,
     password character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    email character varying(255) COLLATE pg_catalog."default",
     role character varying(255) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (user_id)
 );
