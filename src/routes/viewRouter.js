212876765e81a37a347df224425e447068cc98e6
Montse Garcia <montsegarciaj@hotmail.com>
2026-04-22 13:14:42 +0200

    vistas de tasks

--- src/routes/viewRouter.js
+++ src/routes/viewRouter.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import viewRouterUser from "./views/viewRouterUser.js";
+import viewRouterTask from "./views/viewRouterTasks.js";
+
 
 const router = Router();
 
 router.use("/users", viewRouterUser);
+router.use("/tasks", viewRouterTask);
 
 export default router;
