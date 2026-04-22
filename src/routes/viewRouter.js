82e37830ea48f19d1b8214a578ea85b473796304
Montse Garcia <montsegarciaj@hotmail.com>
2026-04-22 15:27:29 +0200

    vistas de evaluations

--- src/routes/viewRouter.js
+++ src/routes/viewRouter.js
@@ -2,12 +2,15 @@ import { Router } from "express";
 import viewRouterUser from "./views/viewRouterUser.js";
 import viewRouterProject from "./views/viewRouterProject.js";
 import viewRouterTask from "./views/viewRouterTasks.js";
+import viewRouterEvaluations from "./views/viewRouterEvaluations.js";
+
 
 const router = Router();
 
 router.use("/users", viewRouterUser);
 router.use("/projects", viewRouterProject);
 router.use("/tasks", viewRouterTask);
+router.use("/evaluations", viewRouterEvaluations);
 
 
 export default router;
