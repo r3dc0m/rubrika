4f25b5446a29e2b960357b8444825dde17341db1
saray <saragaby314@gmail.com>
2026-04-22 15:44:37 +0200

    feat: vistas de criteria

--- src/routes/viewRouter.js
+++ src/routes/viewRouter.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import viewRouterUser from "./views/viewRouterUser.js";
 import viewRouterProject from "./views/viewRouterProject.js";
-import viewRouterTask from "./views/viewRouterTasks.js";
+import viewRouterTasks from "./views/viewRouterTasks.js";
+import viewRouterCriteria from "./views/viewRouterCriteria.js";
 
 const router = Router();
 
 router.use("/users", viewRouterUser);
 router.use("/projects", viewRouterProject);
-router.use("/tasks", viewRouterTask);
-
+router.use("/tasks", viewRouterTasks);
+router.use("/criteria", viewRouterCriteria);
 
 export default router;
